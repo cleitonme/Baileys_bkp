59347d0421fdda711f114c2941a68fcbe32da498
cleiton meurer <cleitonme@gmail.com>
2024-12-26 13:02:49 -0300

    sync history

--- src/Utils/history.ts
+++ src/Utils/history.ts
@@ -38,6 +38,7 @@ export const processHistoryMessage = (item: proto.IHistorySync) => {
 	case proto.HistorySync.HistorySyncType.INITIAL_BOOTSTRAP:
 	case proto.HistorySync.HistorySyncType.RECENT:
 	case proto.HistorySync.HistorySyncType.FULL:
+	case proto.HistorySync.HistorySyncType.ON_DEMAND:
 		for(const chat of item.conversations! as Chat[]) {
 			contacts.push({ id: chat.id, name: chat.name || undefined })
 
@@ -62,7 +63,7 @@ export const processHistoryMessage = (item: proto.IHistorySync) => {
 
 				if(
 					(message.messageStubType === WAMessageStubType.BIZ_PRIVACY_MODE_TO_BSP
-					|| message.messageStubType === WAMessageStubType.BIZ_PRIVACY_MODE_TO_FB
+						|| message.messageStubType === WAMessageStubType.BIZ_PRIVACY_MODE_TO_FB
 					)
 					&& message.messageStubParameters?.[0]
 				) {
@@ -93,6 +94,8 @@ export const processHistoryMessage = (item: proto.IHistorySync) => {
 		chats,
 		contacts,
 		messages,
+		syncType: item.syncType,
+		progress: item.progress
 	}
 }
 
